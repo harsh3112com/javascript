edf6501da2a1436637d0d28fe9fcd98e468b2f5c
harsh3112com <harshbedi3112@gmail.com>
2025-02-05 19:09:54 +0530

    js

--- json/json01.js
+++ json/json01.js
@@ -1,11 +1,21 @@
+// async function apifetch() {
+//     let data = await fetch ("http://localhost:3000/student")
+//     let final_data =await data.json()
+// console.log(final_data);
+
+// }
+// apifetch()
+
 async function apifetch() {
     let data = await fetch ("http://localhost:3000/student")
     let final_data =await data.json()
     let tdata = final_data.map((e)=>`
        <tr>
+         <td> ${e.name}</td>
+         <td> ${e.age}</td>
+         <td> ${e.contact}</td>
+          <td> ${e.city}</td>
          <td> ${e.id}</td>
-         <td> ${e.title}</td>
-         <td> ${e.body}</td>
       </tr>
     
     `).join("")
